80ae8f3208a5ef82e227a5e6b9c4f5910de2ac8b
JaViKo <javikogutierrez64@gmail.com>
2023-05-20 23:53:13 -0500

    get items

--- src/layouts/test_lay/src/index.ts
+++ src/layouts/test_lay/src/index.ts
@@ -1,6 +1,8 @@
-import { ref } from 'vue';
-import { defineLayout } from '@directus/extensions-sdk';
+import { computed, ref, toRefs } from 'vue';
+import { defineLayout, useCollection, useItems, useSync } from '@directus/extensions-sdk';
 import LayoutComponent from './layout.vue';
+import { syncRefProperty } from './utils/sync-ref-property';
+import { Field } from '@directus/types';
 
 export default defineLayout({
 	id: 'test_lay',
@@ -12,9 +14,52 @@ export default defineLayout({
 		sidebar: () => null,
 		actions: () => null,
 	},
-	setup() {
-		const name = ref('Custom Layout');
+	setup(props, { emit }) {
+		const layoutQuery: any = useSync(props, 'layoutQuery', emit);
+		const { collection, filter, filterUser, search } = toRefs(props);
+		const { info, primaryKeyField, fields: fieldsInCollection, sortField } = useCollection(collection as any);
+		const { sort, limit, page, fields } = useItemOptions();
+		console.log({ sort, limit, page, fields });
+		
+		const query: any = {
+			fields: fields,
+			limit, 
+			sort, 
+			search, 
+			filter, 
+			page
+		};
+		const {
+			items,
+			loading,
+			error,
+			totalPages,
+			itemCount,
+			totalCount,
+			changeManualSort,
+			getItems,
+			getItemCount,
+			getTotalCount,
+		} = useItems(collection as any, query);
+		console.log('<--------------- JK Index --------------->');
+		console.log(items);
+		return { items };
+		
+		function useItemOptions() {
+			const page = syncRefProperty<any, any>(layoutQuery, 'page'  , 1);
+			const limit = syncRefProperty<any, any>(layoutQuery, 'limit', 25);
+			const defaultSort = computed(() => (primaryKeyField.value ? [primaryKeyField.value?.field] : []));
+			const sort = syncRefProperty<any, any>(layoutQuery, 'sort', defaultSort);
 
-		return { name };
+			const fieldsDefaultValue = computed(() => {
+				return fieldsInCollection.value
+					.filter((field: Field) => !field.meta?.hidden)
+					.slice(0, 4)
+					.map(({ field }: Field) => field)
+					.sort();
+			});
+			const fields = syncRefProperty<any, any>(layoutQuery, 'fields', fieldsDefaultValue);
+			return { sort, limit, page, fields };
+		}
 	},
 });
--- src/layouts/test_lay/src/layout.vue
+++ src/layouts/test_lay/src/layout.vue
@@ -1,24 +1,49 @@
+import { toRefs } from 'vue';
 <template>
-	<div>
-		<p>Name: {{ name }}</p>
-		<p>Collection: {{ collection }}</p>
+	<div class="layout-tabular">
+		<v-table
+			ref="table"
+			class="table"
+			show-resize
+			must-sort
+			:items="items"
+			selection-use-keys
+		>
+			<!-- <template v-for="header in tableHeaders" :key="header.value" #[`item.${header.value}`]="{ item }">
+				<render-display
+					:value="getFromAliasedItem(item, header.value)"
+					:display="header.field.display"
+					:options="header.field.displayOptions"
+					:interface="header.field.interface"
+					:interface-options="header.field.interfaceOptions"
+					:type="header.field.type"
+					:collection="header.field.collection"
+					:field="header.field.field"
+				/>
+			</template> -->
+
+		</v-table>
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+	import { toRefs } from 'vue';
+	import { Item } from '@directus/types';
+import { useCollection, useStores } from '@directus/extensions-sdk';
 
-export default defineComponent({
-	inheritAttrs: false,
-	props: {
-		collection: {
-			type: String,
-			required: true,
-		},
-		name: {
-			type: String,
-			required: true,
-		},
-	},
-});
+	interface Props {
+		collection: string;
+		items: Item[];
+		tableHeaders: any[];
+	}
+
+	const props = withDefaults(defineProps<Props>(), {});
+
+	const { collection } = toRefs(props);
+
+	console.log('<--------------- JK Layout --------------->');
+	const { useCollectionsStore } = useStores();
+	console.log(useStores());
+	console.log(useCollectionsStore());
+	// const { getFromAliasedItem } = useAliasFields(fieldsWritable, collection);
 </script>
